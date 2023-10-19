163ecb2a863afa47a8182470894d62894ad6ceef
Liherbert <liherbert.lisboa@gmail.com>
2023-10-19 13:14:14 -0300

    create component ImageSlider and insert images UE

---
+++ scripts/components/ImageSlider.js
@@ -0,0 +1,29 @@
+// Pecorre todos os slides e atualiza a imagem inicial do slide
+slides.forEach((slider) => {
+  slider.currentImage = document.getElementById(slider.idTag).src =
+    slider.imagesSrc[0];
+});
+
+// Avançar imagem do Slide
+function nextImage(currentSlide) {
+  let size = currentSlide.imagesSrc.length - 1;
+
+  if (currentSlide.index < size) {
+    currentSlide.index = currentSlide.index + 1;
+    setCurrentImage(currentSlide);
+  }
+}
+
+// retornar imagem do Slide
+function previousImage(currentSlide) {
+  if (currentSlide.index > 0) {
+    currentSlide.index = currentSlide.index - 1;
+    setCurrentImage(currentSlide);
+  }
+}
+
+// Atualizar a imagem atual do slide
+function setCurrentImage(currentSlide) {
+  currentSlide.currentImage = document.getElementById(currentSlide.idTag).src =
+    currentSlide.imagesSrc[currentSlide.index];
+}
---
+++ scripts/components/ImagesSlidersUE.js
@@ -0,0 +1,33 @@
+//adicionar em array a idTag do slide e suas respectivas src das imagens.
+var slides = [
+  {
+    idTag: "ue4-0",
+    index: 0,
+    currentImage: "",
+    imagesSrc: [
+      "images/ue/UE 4/Imagem 2 - UE4 1.jpg",
+      "images/ue/UE 4/Imagem 2 - UE4 2.jpg",
+      "images/ue/UE 4/Imagem 2 - UE4 3.jpg",
+    ],
+  },
+  {
+    idTag: "ue14-1",
+    index: 0,
+    currentImage: "",
+    imagesSrc: [
+      "images/ue/UE 14/_Slide 1 - 1.png",
+      "images/ue/UE 14/_Slide 1 - 2.png",
+      "images/ue/UE 14/_Slide 1 - 3.png",
+    ],
+  },
+  {
+    idTag: "ue14-2",
+    index: 0,
+    currentImage: "",
+    imagesSrc: [
+      "images/ue/UE 14/_Slide 2 - 1.png",
+      "images/ue/UE 14/_Slide 2 - 2.png",
+      "images/ue/UE 14/_Slide 2 - 3.png",
+    ],
+  },
+];
